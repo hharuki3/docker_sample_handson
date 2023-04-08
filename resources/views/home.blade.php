b3ef268443ed93959616d51d9529785e192ea68a
hharuki3 <pingyanchunshu@gmail.com>
2023-04-08 11:57:40 +0900

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -36,28 +36,37 @@
 @endsection
 @section('content')
 
-@if(!$phrase_exists)
-<a href="{{route('create')}}">フレーズを登録しよう！</a>
-@endif
-@foreach($phrases as $phrase)
-    <div>
-        <th scope="row" style="display:inline-flex">{{$phrase['japanese']}}</th>
-    </div>
-        <th scope="row" style="display:inline-flex">{{$phrase['phrase']}}</th>
+<div class="row">
 
-    <a href="detail/{{$phrase['id']}}" class="btn btn-light">詳細</a>
-    <a href="edit/{{$phrase['id']}}" class="btn btn-light">編集</a>
-    <div style="display:inline-flex">
- 
-        
-        <form action="{{route('destroy')}}" method="post"  id="delete-form-{{$phrase['id']}}">
-            @csrf
-            <input type="hidden" name="phrase_id" value="{{ $phrase['id'] }}" >
-            <button type="submit" class="btn btn-light"  onclick="deleteHandle(event,{{ $phrase['id'] }} )">削除</button>
-        </form>
-        
+    @if(!$phrase_exists)
+    <a href="{{route('create')}}">フレーズを登録しよう！</a>
+    @endif
+
+
+    @foreach($phrases as $phrase)
+    <div class="col-md-8 justify-content-center">
+        <div>
+            <th scope="row" style="display:inline-flex">{{$phrase['japanese']}}</th>
+        </div>
+            <th scope="row" style="display:inline-flex">{{$phrase['phrase']}}</th>  
     </div>
-@endforeach
+
+    <div class="col-md-4">
+        <a href="detail/{{$phrase['id']}}" class="btn btn-light">詳細</a>
+        <a href="edit/{{$phrase['id']}}" class="btn btn-light">編集</a>
+        <div style="display:inline-flex">
+            <form action="{{route('destroy')}}" method="post"  id="delete-form-{{$phrase['id']}}">
+                @csrf
+                <input type="hidden" name="phrase_id" value="{{ $phrase['id'] }}" >
+                <button type="submit" class="btn btn-light"  onclick="deleteHandle(event,{{ $phrase['id'] }} )">削除</button>
+            </form>
+        </div>
+    </div>
+
+    @endforeach
+
+</div>
+
 
 
 @endsection
